1ab82a1d389db359495a822170db29090a85fcac
Mike Petersen <ekim5597@yahoo.com>
2016-09-16 20:47:14 -0500

    Added formatter xml--use it for Eclipse

---
+++ src/hack_isu_f16/main/Main.java
@@ -0,0 +1,11 @@
+package hack_isu_f16.main;
+
+public class Main
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, world. Goodbye, world.");
+  }
+
+}
--- src/hack_isu_f16/util/Utils.java
+++ src/hack_isu_f16/util/Utils.java
@@ -1,7 +1,10 @@
 package hack_isu_f16.util;
 
-public class Utils {
-	private Utils() { /* No */ }
-	
-	
+public class Utils
+{
+  private Utils()
+  {
+    /* No */
+  }
+
 }
