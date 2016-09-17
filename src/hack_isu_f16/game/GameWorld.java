ec71ef3cd93d07915efe2188e8942896460cef65
Mike Petersen <mapsmap55@yahoo.com>
2016-09-17 12:05:30 -0500

    Some things

--- src/hack_isu_f16/game/GameWorld.java
+++ src/hack_isu_f16/game/GameWorld.java
@@ -1,6 +1,35 @@
 package hack_isu_f16.game;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
 public class GameWorld
 {
+  enum TileType
+  {
+    AIR,
+    FALLING,
+    BREAKABLE,
+    NORMAL
+  }
 
+  /**
+   * The tiles of the game world. LinkedLists are used to allow deque operations
+   * to be performed in constant time if necessary.
+   */
+  private LinkedList<TileType>[] tiles;
+  private Set<Entity> entities;
+
+  private static final int HEIGHT = 24;
+
+  public GameWorld()
+  {
+    tiles = (LinkedList<TileType>[]) new LinkedList[HEIGHT];
+    for (int i = 0; i < tiles.length; ++i)
+    {
+      tiles[i] = new LinkedList<>();
+    }
+    entities = new HashSet<>();
+  }
 }
--- src/hack_isu_f16/util/Utils.java
+++ src/hack_isu_f16/util/Utils.java
@@ -6,9 +6,67 @@ public class Utils
   {
     /* No */
   }
-  
+
   public static boolean equalsApprox(double a, double b, double maxDelta)
   {
     return Math.abs(a - b) <= maxDelta;
+  }
+
+  public static class RepeatingRunnable implements Runnable
+  {
+    private Runnable toRun;
+    private volatile boolean running;
+    private final long millis;
+    private final int nanos;
+
+    public RepeatingRunnable(Runnable toRun, long millis)
+    {
+      this(toRun, millis, 0);
+    }
+    
+    public RepeatingRunnable(Runnable toRun, long millis, int nanos)
+    {
+      this.toRun = toRun;
+      this.millis = millis;
+      this.nanos = nanos;
+    }
+
+    public void run()
+    {
+      running = true;
+      long t;
+      long m;
+      int n;
+
+      while (running)
+      {
+        t = System.nanoTime();
+        toRun.run();
+        t = System.nanoTime() - t;
+
+        m = millis - t / 1000000;
+        n = nanos - (int) (t % 1000000);
+
+        while (n < 0)
+        {
+          --m;
+          n += 1000000;
+        }
+
+        try
+        {
+          Thread.sleep(m, n);
+        }
+        catch (InterruptedException e)
+        {
+
+        }
+      }
+    }
+
+    public void stop()
+    {
+      running = false;
+    }
   }
 }
