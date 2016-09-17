8991cbbae7a1a6d147fab437286002b2a10e8ac3
Mike Petersen <mapsmap55@yahoo.com>
2016-09-17 13:21:28 -0500

    Initial code for worldgen

--- src/hack_isu_f16/game/GameWorld.java
+++ src/hack_isu_f16/game/GameWorld.java
@@ -1,8 +1,15 @@
 package hack_isu_f16.game;
 
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.Random;
 import java.util.Set;
+import java.util.function.Function;
+
+import hack_isu_f16.util.Utils;
 
 public class GameWorld
 {
@@ -31,5 +38,64 @@ public class GameWorld
       tiles[i] = new LinkedList<>();
     }
     entities = new HashSet<>();
+    
+    initGen();
+  }
+  
+  public void generateNextColumn()
+  {
+    generateNextColumn(true);
+  }
+  
+  private void generateNextColumn(boolean removeFirst)
+  {
+    Random r = new Random();
+    TileType[] values = Utils.ArrayUtils.asArray(TileType.AIR, TileType.FALLING, TileType.BREAKABLE, TileType.NORMAL);
+    for (int i = 0; i < tiles.length; ++i)
+    {
+      LinkedList<TileType> l = tiles[i];
+      TileType last = l.getLast();
+      int[] weights;
+      switch (last)
+      {
+        case AIR:
+          weights = new int[] { 8, 1, 3, 2 };
+          break;
+        case FALLING:
+          weights = new int[] { 2, 5, 3, 1 };
+          break;
+        case BREAKABLE:
+          weights = new int[] { 4, 1, 6, 0 };
+          break;
+        case NORMAL:
+          weights = new int[] { 3, 6, 4, 8 };
+          break;
+        default:
+          weights = new int[] { 0, 0, 0, 1 };
+          break;
+      }
+      l.addLast(Utils.weightedRandom(r, weights, values));
+      if (removeFirst)
+      {
+        l.removeFirst();
+      }
+    }
+  }
+  
+  private void initGen()
+  {
+    final int WIDTH = 20;
+    for (int w = 0; w < WIDTH / 4; ++w)
+    {
+      for (int i = 0; i < tiles.length - 1; ++i)
+      {
+        tiles[i].add(TileType.AIR);
+      }
+      tiles[tiles.length - 1].add(TileType.NORMAL);
+    }
+    for (int w = WIDTH / 4; w < WIDTH; ++w)
+    {
+      generateNextColumn(false);
+    }
   }
 }
--- src/hack_isu_f16/util/Utils.java
+++ src/hack_isu_f16/util/Utils.java
@@ -1,5 +1,13 @@
 package hack_isu_f16.util;
 
+import java.awt.image.BufferedImage;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Predicate;
+
 public class Utils
 {
   private Utils()
@@ -10,6 +18,103 @@ public class Utils
   public static boolean equalsApprox(double a, double b, double maxDelta)
   {
     return Math.abs(a - b) <= maxDelta;
+  }
+
+  public static class ArrayUtils
+  {
+    public static Integer[] asWrapperArray(int... arr)
+    {
+      Integer[] toRet = new Integer[arr.length];
+      for (int i = 0; i < arr.length; ++i)
+      {
+        toRet[i] = arr[i];
+      }
+      return toRet;
+    }
+    
+    public static int[] asPrimitiveArray(Integer... arr)
+    {
+      int[] toRet = new int[arr.length];
+      for (int i = 0; i < arr.length; ++i)
+      {
+        toRet[i] = arr[i] == null ? 0 : arr[i];
+      }
+      return toRet;
+    }
+    
+    @SafeVarargs
+    public static <T> T[] asArray(T... values)
+    {
+      return Arrays.copyOf(values, values.length);
+    }
+    
+    public static <T> int indexOf(Object arr, Predicate<? super T> pred)
+    {
+      int l = Array.getLength(arr);
+      for (int i = 0; i < l; ++i)
+      {
+        if (pred.test((T) Array.get(arr, i)))
+        {
+          return i;
+        }
+      }
+
+      return -1;
+    }
+
+    public static int sum(int... nums)
+    {
+      int sum = 0;
+      for (int i = 0; i < nums.length; ++i)
+      {
+        sum += nums[i];
+      }
+
+      return sum;
+    }
+  }
+
+  public static <T> T weightedRandom(Random r, int[] weights, T[] choices)
+  {
+    if (weights.length > choices.length)
+    {
+      throw new IllegalArgumentException("Length mismatch");
+    }
+    if (ArrayUtils.indexOf(weights, (Integer t) -> t < 0) != -1)
+    {
+      throw new IllegalArgumentException("Negative weight");
+    }
+    int c = r.nextInt(ArrayUtils.sum(weights));
+    int s = 0;
+    for (int i = 0; i < weights.length; ++i)
+    {
+      s += weights[i];
+      if (c < s)
+      {
+        return choices[i];
+      }
+    }
+
+    return null;
+  }
+
+  public static BufferedImage[] splitImage(BufferedImage img, int rows, int cols)
+  {
+    BufferedImage[] toRet = new BufferedImage[rows * cols];
+    int w = img.getWidth() / cols;
+    int h = img.getHeight() / rows;
+
+    for (int r = 0; r < rows; ++r)
+    {
+      for (int c = 0; c < cols; ++c)
+      {
+        BufferedImage im = new BufferedImage(w, h, img.getType());
+        im.getGraphics().drawImage(img, c * -w, r * -h, null);
+        toRet[r * rows + c] = im;
+      }
+    }
+
+    return toRet;
   }
 
   public static class RepeatingRunnable implements Runnable
@@ -23,7 +128,7 @@ public class Utils
     {
       this(toRun, millis, 0);
     }
-    
+
     public RepeatingRunnable(Runnable toRun, long millis, int nanos)
     {
       this.toRun = toRun;
